61105faa8e7b8ed1ad4e68114d3c057b18bddc62
saba3013 <tamttamt16@gmail.com>
2024-09-13 23:29:33 +0400

    eighth commit

---
+++ day 11/homework/homework.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter a first number: "))
+num2 = int(input("Please enter a second number: "))
+print(num1 == num2)
+
+age = int(input("Please enter your age: "))
+print(age > 18)
