3ce18ad70ad8baeb71db195b8503dd6d7247fee2
saba3013 <tamttamt16@gmail.com>
2024-09-28 16:48:36 +0400

    nineth commit

---
+++ day 21/classwork/classwork.py
@@ -0,0 +1,64 @@
+
+
+
+# 1)
+
+# .append()- ფუნქცია ამატებს გადმოცემულ ელემენტს სიის ბოლოში.
+# .insert()- ფუნქცია ამტებს გადმოცემულ ელემენტს კონკრეტულ ინდექსზე.
+
+# 2)
+
+surname = input("Please enter your surname: ")
+print(len(surname))
+
+# 3)
+
+movies = ["Avatar", "Titanic", "Avengers", "GOA" , "Group 30"]
+
+movies.insert(3, "Saba")
+
+print(movies)
+
+# 4)
+
+cars = ["BMW",  "Audi", "Honda"]
+
+cars.append("Mersedes")
+print(cars)
+
+# 5)
+
+names = ["dato", "nika", "luka" , "gabrieli" , "akaki" , "gocha"]
+
+names.pop(4)
+
+
+
+
+name = input("Please enter your name: ")
+
+print(name[:2] + name[3:])
+
+# 2)
+
+countrys = ["Georgia", "Latvia", "France", "Japan"]
+print(countrys.pop(1))
+print(countrys)
+
+# 3)
+
+numbers = [10, 20, 30]
+numbers.insert(1, 15)
+print(numbers)
+
+# 4)
+list1 = []
+print(list1)
+list1.append("Dog")
+list1.append("Cat")
+list1.append("Bird")
+print(list1)
+
+
+
+
---
+++ day 22/homework/homework.py
@@ -0,0 +1,23 @@
+name = input("Please enter your name: ")
+
+print(name[:2] + name[3:])
+
+# 2)
+
+countrys = ["Georgia", "Latvia", "France", "Japan"]
+print(countrys.pop(1))
+print(countrys)
+
+# 3)
+
+numbers = [10, 20, 30]
+numbers.insert(1, 15)
+print(numbers)
+
+# 4)
+list1 = []
+print(list1)
+list1.append("Dog")
+list1.append("Cat")
+list1.append("Bird")
+print(list1)
